264932b26052f7dc59984e0b5083cee7535917b7
edenbam <edenbam@gmail.com>
2022-06-21 20:37:23 -0300

    Criando as classes de conexão e sessão

---
+++ pythools/tests/test_usuarios.py
@@ -0,0 +1,28 @@
+from pythools.spam.db import Conexao
+from pythools.spam.modelos import Usuario
+
+
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='Renzo')
+    sessao.salvar(usuario)
+
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
+
+
+def test_listar_usuarios():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
+
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+
+    assert usuarios == sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
