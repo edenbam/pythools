e88a7da0827d98e3acc79a5c444c6122e1c0f622
edenbam <edenbam@gmail.com>
2022-06-21 14:58:20 -0300

    Criando testes para excessões

--- pythools/tests/test_enviador_de_email.py
+++ pythools/tests/test_enviador_de_email.py
@@ -1,13 +1,36 @@
-from pythools.spam.enviador_de_email import Enviador
+import pytest
+
+from pythools.spam.enviador_de_email import Enviador, EmailInvalido
+
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
-def test_remetente():
+@pytest.mark.parametrize(
+    'destinatario',
+    ['edenbam@gmail.com','foo@bar.com.br']
+)
+
+def test_remetente(destinatario):
     enviador = Enviador()
-    resultado = enviador.enviar('edenbam@gmail.com',
+    resultado = enviador.enviar(destinatario,
                     'emial@test.com',
                     'Cursos python',
                     'Ultima turma')
-    assert 'edenbam@gmail.com' in resultado
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['','foo']
+)
+
+def test_remetente_invalido(destinatario):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(destinatario,
+            'emial@test.com',
+            'Cursos python',
+            'Ultima turma'
+        )
